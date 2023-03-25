6c5be79678f26d8cf8d32ea7e2215d49c4b2de8d
flatbreadd <125706793+flatbreadd@users.noreply.github.com>
2023-03-25 13:24:41 -0400

    Update Worms Code.py

--- Worms Code.py
+++ Worms Code.py
@@ -1,4 +1,4 @@
-Just copy and paste in your python editor when you are done remove line 1 and line 2 which is the line you are reading now also dont forget to do py install pygame 
+Just copy and paste in your python editor when you are done remove line 1 and line 2 dont forget to do pip install pygame 
 i recommend using pycharm and go to command prompt not local
 import pygame as pg
 from random import randrange
